bdf7e92acd206171bb59b1348469c22970fb14cf
reza <moradzade.mohamadreza@gmail.com>
2025-06-07 16:46:57 -0700

    third commit

---
+++ lib/screens/authentication_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class AuthenticationScreen extends StatelessWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/ax.png', width: 500, height: 250),
+              ClipOval(
+                child: Image.asset(
+                  'assets/images/img_2.png',
+                  width: 140,
+                  height: 140,
+                ),
+              ),
+              SizedBox(height: 22),
+              Text(
+                'Moradzadeh',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 33),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  minimumSize: Size(307, 45),
+                ),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                onPressed: () {},
+              ),
+              SizedBox(height: 35),
+              Text(
+                'Switch account',
+                style: TextStyle(
+                  color: Color(0xff4AAFF6),
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Dont't have an account?",
+                    style: TextStyle(
+                      color: Colors.white.withAlpha(120),
+                      fontSize: 19,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
